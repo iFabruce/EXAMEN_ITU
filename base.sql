bbc06a55cda2d782cd56bbd4fc0e9fb9dc1e61c9
Tahiana Andriambahoaka <89937368+TahianaAndriambahoaka@users.noreply.github.com>
2022-03-24 14:54:33 +0300

    Update base.sql

--- base.sql
+++ base.sql
@@ -1,3 +1,5 @@
+DROP VIEW Prix_reviens;
+DROP VIEW Total_ingredient;
 DROP VIEW Menu;
 
 DROP TABLE Addition;
@@ -9,6 +11,7 @@ DROP TABLE Recette;
 DROP TABLE Prix_produit;
 DROP TABLE Produit;
 DROP TABLE Categorie;
+DROP TABLE Prix_ingredient;
 DROP TABLE Ingredient;
 DROP TABLE Rel_table_client;
 DROP TABLE Tables;
@@ -34,6 +37,12 @@ CREATE TABLE Rel_table_client (
 CREATE TABLE Ingredient (
     id SERIAL PRIMARY KEY,
     nom VARCHAR(20)
+);
+
+CREATE TABLE Prix_ingredient (
+    id_ingredient INT REFERENCES Ingredient(id),
+    montant DOUBLE PRECISION,
+    date DATE
 );
 
 CREATE TABLE Categorie (
@@ -98,8 +107,22 @@ CREATE VIEW Menu AS
 SELECT  p.id as id_produit, p.nom as produit, c.nom as categorie, pp.montant as prix, pp.date
         FROM Produit p
         JOIN Prix_produit pp ON( pp.id_produit=p.id)
-        JOIN Categorie c ON (c.id=p.id_categorie);
+        JOIN Categorie c ON (c.id=p.id_categorie)
+        WHERE pp.date = (select max(date) from Prix_produit);
 
+CREATE VIEW Total_ingredient AS
+SELECT  p.*, SUM(pi.montant*r.quantite) as montant
+        FROM Produit p
+        JOIN Recette r ON (r.id_produit = p.id)
+        JOIN Ingredient i ON (i.id = r.id_ingredient)
+        JOIN Prix_ingredient pi ON (pi.id_ingredient = i.id)
+        WHERE pi.date = (select max(date) from Prix_ingredient)
+        GROUP BY p.id;
+
+CREATE VIEW Prix_reviens AS
+SELECT  ti.id as id_produit, ti.nom as nom_produit, m.prix-ti.montant as prix_reviens
+        FROM Total_ingredient ti
+        JOIN Menu m ON (m.id_produit = ti.id);
 
 INSERT INTO Categorie VALUES
 (DEFAULT, 'Entree'),
@@ -120,3 +143,30 @@ INSERT INTO Prix_produit VALUES
 (3, 2500, '2022-03-22'),
 (4, 3500, '2022-03-22'),
 (5, 4000, '2022-03-22');
+
+INSERT INTO Ingredient VALUES
+(DEFAULT, 'farine'),
+(DEFAULT, 'poivre'),
+(DEFAULT, 'sucre'),
+(DEFAULT, 'sel'),
+(DEFAULT, 'carotte');
+
+INSERT INTO Prix_ingredient VALUES
+(1, 100, '2022-03-22'),
+(2, 200, '2022-03-22'),
+(3, 300, '2022-03-22'),
+(4, 150, '2022-03-22'),
+(5, 250, '2022-03-22');
+
+INSERT INTO Recette VALUES
+(1, 1, 2),
+(1, 2, 3),
+(2, 1, 4),
+(2, 2, 5),
+(2, 3, 2),
+(3, 4, 2),
+(3, 5, 1),
+(4, 1, 4),
+(4, 2, 2),
+(4, 3, 1),
+(5, 1, 8);
